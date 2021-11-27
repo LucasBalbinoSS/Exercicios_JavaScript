af5c987bc67ab8f2de5dc7656e561aebd5bfc7d6
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2021-11-26 23:24:04 -0300

    Comando console.log

---
+++ aula03/index.js
@@ -0,0 +1,10 @@
+// Sempre usaremos essa função em Javascript, serve para imprimir //
+console.log('Lucas Balbino');
+
+// Podemos utilizar aspas simples
+console.log("Lucas \"Balbino\"");
+
+// Também podemos usar crases
+console.log(`Lucas Balbino`);
+
+console.log(346, 67.5);
