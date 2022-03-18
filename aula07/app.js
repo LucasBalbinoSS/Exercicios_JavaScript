8a4aba40f40b9bd776fbb193c5cabb82cecf7844
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2022-03-18 18:37:51 -0300

    Conditions with JavaScript

---
+++ aula07/app.js
@@ -0,0 +1,11 @@
+let userAge = parseInt(prompt('Qual é a sua idade?'))
+
+if (userAge >= 0 && userAge <= 5) {
+    console.log('Passe grátis');
+} else if (userAge >= 5 && userAge <= 10) {
+    console.log('Passe de criança')
+} else if (userAge >= 10 && userAge <= 65) {
+    console.log('Passe de adulto');
+} else if (userAge >= 65) {
+    console.log('Passe de sênior, tem R$10,00 de desconto');
+}
