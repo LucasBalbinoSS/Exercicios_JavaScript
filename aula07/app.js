39c411993983d32f014fbe306eb3cae4aaeae537
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2022-03-18 23:38:46 -0300

    Ajustando condições

--- aula07/app.js
+++ aula07/app.js
@@ -4,7 +4,7 @@ if (userAge >= 0 && userAge <= 5) {
     console.log('Passe grátis');
 } else if (userAge >= 5 && userAge <= 10) {
     console.log('Passe de criança')
-} else if (userAge >= 10 && userAge <= 65) {
+} else if (userAge >= 10 && userAge < 65) {
     console.log('Passe de adulto');
 } else if (userAge >= 65) {
     console.log('Passe de sênior, tem R$10,00 de desconto');
